a504b328edaf1483cbfe0c0903b98fcc05a5b547
kriyona infotech <kriyonainfotech@gmail.com>
2026-04-12 10:35:16 +0530

    save

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://lotus-delta-six.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
